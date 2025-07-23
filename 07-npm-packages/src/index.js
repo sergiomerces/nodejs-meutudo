889298a89dc230e39fec3968353d7119548c6cf3
sergiomerces <sergio.merces@gmail.com>
2025-07-23 01:53:05 -0300

    gerenciando dependencias

--- 07-npm-packages/src/index.js
+++ 07-npm-packages/src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello NodeJS');
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+import symbols from "log-symbols";
+
+console.log(logSymbols.success, chalk.green.italic('Servidor iniciado com sucesso'));
+console.log(logSymbols.error, chalk.red.bold('Erro no login de conexão'));
